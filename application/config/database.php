e509a0ec9b77e0da097a934654017e3ccf3894f6
Tupay <lutfi@mhs.ubharajaya.ac.id>
2017-11-01 16:04:03 +0700

    Update remove config db for frontend

--- application/config/database.php
+++ application/config/database.php
@@ -74,12 +74,12 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 $db['default'] = array(
-	'dsn'	   => 'sqlsrv:Server=localhost;Database=ADMS_DEV;',
-	'hostname' => 'ibiddev.database.windows.net',
-	'username' => 'adminibiddev',
-	'password' => 'Serasi123',
-	'database' => 'Stock',
-	'dbdriver' => 'sqlsrv',
+	'dsn'	   => '',
+	'hostname' => '',
+	'username' => '',
+	'password' => '',
+	'database' => '',
+	'dbdriver' => '',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
