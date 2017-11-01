170417d7b66030163828ecc37482b1ffc6358a0c
Tupay <lutfi@mhs.ubharajaya.ac.id>
2017-11-01 19:15:08 +0700

    update autoload

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('session','form_validation');
+$autoload['libraries'] = array('session'); 
 
 /*
 | -------------------------------------------------------------------
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url','json_output','adms');
+$autoload['helper'] = array('url','form' , 'adms','global');
 
 /*
 | -------------------------------------------------------------------
@@ -117,7 +117,7 @@ $autoload['config'] = array();
 | "codeigniter_lang.php" would be referenced as array('codeigniter');
 |
 */
-$autoload['language'] = array('system');
+$autoload['language'] = array();
 
 /*
 | -------------------------------------------------------------------
