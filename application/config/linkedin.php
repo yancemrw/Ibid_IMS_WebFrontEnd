5ede75ac97903c9030e39c66079ea3fdb93c1d9c
Tupay <lutfi@mhs.ubharajaya.ac.id>
2017-11-02 13:35:21 +0700

    Bugsfix linked in

--- application/config/linkedin.php
+++ application/config/linkedin.php
@@ -8,7 +8,7 @@ $config = array(
 		'API_KEY' => '81o3npu3d7wt4d',
 		'SECRET_KEY' => 'VIfJ9bU6CP017yps',
 		'SITE_URL' => 'http://localhost:8888/lutfi/linkedin/pertama/',
-		'REDIRECT_URL' => 'http://localhost:8888/ibiddevelopment/ibiddevapi/ibidadmsuser/index.php/omni/linkedin/index',  
+		'REDIRECT_URL' => 'http://ibidadmsdevwebfront.azurewebsites.net/index.php/omni/linkedin/index',  
 		'SCOPE' => 'r_basicprofile r_emailaddress', 
 		)
 	);
