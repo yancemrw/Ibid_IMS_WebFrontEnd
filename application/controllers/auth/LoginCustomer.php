f16ccd9221e2ff5b8cf433e28830a50008492507
Andi Fauji Akbar <faujiakbar@gmail.com>
2017-11-08 17:51:56 +0700

    check response

--- application/controllers/auth/LoginCustomer.php
+++ application/controllers/auth/LoginCustomer.php
@@ -64,6 +64,7 @@ class LoginCustomer extends CI_Controller {
 			$method = 'POST';
 			$responseApi = admsCurl($url, $dataLogin, $method);
 			## redirect dan email(belum)
+			print_r($responseApi); die();
 			if ($responseApi['err']) {
 				echo "<hr>cURL Error #:" . $responseApi['err'];
 			} else {
@@ -74,7 +75,6 @@ class LoginCustomer extends CI_Controller {
 					redirect('auth/loginCustomer');
 				} else {
 					// set token on session
-					print_r($res); die();
 					$this->session->set_userdata('idfront', $res->UserId);
 					$this->session->set_userdata('namefront', $res->Name);
 					$this->session->set_userdata('namefront', $res->username);
