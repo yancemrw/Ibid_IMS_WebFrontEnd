b0bfddb34ef63ae98dff2909d24a876b2b50d1b2
Andi Fauji Akbar <faujiakbar@gmail.com>
2017-11-08 16:30:16 +0700

    Test on fly

--- application/controllers/Afterlogin.php
+++ application/controllers/Afterlogin.php
@@ -7,8 +7,8 @@ class Afterlogin extends CI_Controller {
 		parent::__construct();
 		$this->load->library(array('form_validation'));
 		$this->load->helper(array('global' , 'omni'));
-		$this->AccessApi = new AccessApi(array('client_id' => 'ADMS Web', 'client_secret' => '1234567890', 'username' => 'rendhy.wijayanto@sera.astra.co.id'));
-		$this->AccessApi->redirect_url = base_url('auth/loginCustomer');
+		$this->AccessApi = new AccessApi(array('client_id' => 'ADMS Web', 'client_secret' => '1234567890', 'username' => $this->session->userdata('userdata')->username));
+		$this->AccessApi->redirect_url = base_url('index.php/auth/loginCustomer');
 		$this->AccessApi->check_login();
 	}
 
