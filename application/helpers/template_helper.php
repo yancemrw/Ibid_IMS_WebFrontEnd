4c133748c2abd87d6bc44e11943a6c39c7dccc5b
Anggoro <ankghoro@gmail.com>
2018-01-21 23:33:39 +0700

    Update

--- application/helpers/template_helper.php
+++ application/helpers/template_helper.php
@@ -17,7 +17,7 @@ function template($view = '' , $data = '') {
     $data['bgheader'] = base_url('assetsfront/images/background/bg-about-us.jpg');
     $data['class_header'] = 'header-aboutus';
   }
-  else if(@$data['menu_pages'] === 'faq' || $data['menu_pages'] === 'location' || $data['menu_pages'] === 'contact') {
+  else if(@$data['menu_pages'] === 'faq' || @$data['menu_pages'] === 'location' || @$data['menu_pages'] === 'contact') {
     $data['bgheader'] = base_url('assetsfront/images/background/bg-homepage.jpg');
     $data['class_header'] = '';
   }
