a6f7e0faa291298409410d54a495894e5e9989d9
Anggoro <ankghoro@gmail.com>
2018-01-21 16:14:54 +0700

    update

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -166,7 +166,7 @@
     function formatState(state) {
       if (!state.id) { return state.text; }
       var $state = $(
-        '<span ><img sytle="display: inline-block;" src="http://sera-ibid.stagingapps.net/assets/images/icon/' + state.element.value.toLowerCase() + '.png" /> ' + state.text + '</span>'
+        '<span ><img sytle="display: inline-block;" src="<?php base_url('assets/images/icon/'); ?>' + state.element.value.toLowerCase() + '.png" /> ' + state.text + '</span>'
         );
       return $state;
     }
