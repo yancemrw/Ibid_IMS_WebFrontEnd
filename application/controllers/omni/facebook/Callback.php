04fcd6c38457a78069602323b87efec22ff5ce74
Tupay <lutfi@mhs.ubharajaya.ac.id>
2018-01-24 20:04:40 +0700

    Facebook update

--- application/controllers/omni/facebook/Callback.php
+++ application/controllers/omni/facebook/Callback.php
@@ -1,5 +1,9 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); 
 
+if(!session_id()) {
+    session_start();
+}
+
 // meload omni facebook untuk keperluan access token
 require_once  APPPATH.'../omni/facebook/php-sdk-v4/src/Facebook/autoload.php';
 
