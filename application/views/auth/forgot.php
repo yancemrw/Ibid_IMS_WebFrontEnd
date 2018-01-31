d4c65df12da32e302c6b469613bef1f995a8d307
Anggoro <ankghoro@gmail.com>
2018-01-31 13:35:33 +0700

    Update

--- application/views/auth/forgot.php
+++ application/views/auth/forgot.php
@@ -1,38 +1,41 @@
 <section class="section section-success">
-         <div class="container">
-            <div class="row">
-               <div class="col-md-12 text-center">
-                  <div class="box-forgot-pass">
-                     <img src="<?php echo base_url('assetsfront/images/icon/ic-forgot-pass.png') ?>">
-                     <h2>Forgot Your Password</h2>
-                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed</p>
-                     <div class="form-group floating-label">
-                        <input type="email" name="" class="form-control input-custom">
-                        <label class="label-schedule">Email</label>
-                     </div>
-                     <button class="btn btn-green" onclick="location.href='reset-password.html'" type="button">Kirim</button>
-                  </div>
-               </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12 text-center">
+        <div class="box-forgot-pass">
+          <form name="formSubmit" id="form-submit" method="POST" data-provide="validation">
+            <img src="<?php echo base_url('assetsfront/images/icon/ic-forgot-pass.png') ?>">
+            <h2>Forgot Your Password</h2>
+            <p>Masukkan email anda</p>
+            <div class="form-group floating-label">
+              <input type="email" id="email" name="email" class="form-control input-custom" required />
+              <label class="label-schedule">Email</label>
             </div>
-         </div>
-      </section> 
-
- <!-- <body class="min-h-fullscreen bg-img center-vh p-20" style="background-image: url(https://sera.astra.co.id/uploads/contents/1487930089_9j8r7v6bXZ.png);" data-overlay="7">
-
-    <div class="card card-round card-shadowed px-50 py-30 w-400px mb-0" style="max-width: 100%">
-      <h5 class="text-uppercase"><?php echo @$title; ?></h5>
-      <br>
-      <?php echo @$message; ?>
-        <form action="<?php echo site_url('auth/forgot/'); ?>" id="loginForm" method='POST' class="form-type">
-        <div class="form-group">
-          <label for="username">Email</label>
-          <input type="email" class="form-control" id="email" name="email" value="<?php echo set_value('email'); ?>">
-          <?php echo form_error('email'); ?>
-        </div> 
-
-        <div class="form-group">
-            <button class="btn btn-success">Submit</button>
-            <a href="<?php echo site_url('auth/login') ?>" class="btn btn-default ">Cancel</a>
+            <button class="btn btn-green" id="btn-kirim">Kirim</button>
+          </form>
         </div>
-      </form> 
-    </div>  -->
+      </div>
+    </div>
+  </div>
+</section>
+
+<script>
+  $('#btn-kirim').click(function(e) {
+    if($('#email').val() !== '') {
+      e.preventDefault();
+      $('#btn-kirim').attr('disabled', true);
+    }
+  });
+
+  $('input').blur(function() {
+    tmpval = $(this).val();
+    if(tmpval == '') {
+      $(this).addClass('empty');
+      $(this).removeClass('not-empty');
+    }
+    else {
+      $(this).addClass('not-empty');
+      $(this).removeClass('empty');
+    }
+  });
+</script>
