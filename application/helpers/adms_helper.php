7bf593eff76220a81f4ca86f5067837aa88508f3
Loetfi <lutfi@mhs.ubharajaya.ac.id>
2018-01-31 14:18:32 +0700

    AdmsHelperAlpha
    untuk dimaster semua narik ke alpha.

--- application/helpers/adms_helper.php
+++ application/helpers/adms_helper.php
@@ -2,18 +2,18 @@
 
 // kumpulan link services
 function linkservice($services) {
-	// kalo pengen jalan diawanesia komen dibawah di aktifkan saja
-	$BACKEND 	= ('BACKEND' == strtoupper($services)) ? $return = 'http://ibidadmsdevweb.azurewebsites.net/index.php/' : '';
-	$ACCOUNT 	= ('ACCOUNT' == strtoupper($services)) ? $return = 'http://ibidadmsdevserviceaccount.azurewebsites.net/index.php/' : ''; 
-	$NOTIF 		= ('NOTIF' == strtoupper($services)) ? $return = 'http://ibidadmsdevservicenotification.azurewebsites.net/index.php/' : ''; 
-	$TAKSASI 	=  ('TAKSASI' == strtoupper($services)) ? $return = 'http://ibidadmsdevservicetaksasi.azurewebsites.net/index.php/' : ''; 
-	$FRONTEND 	= ('FRONTEND' == strtoupper($services)) ? $return = 'http://ibidadmsdevwebfront.azurewebsites.net/index.php/' : '';
-	$HANDOVER 	= ('HANDOVER' == strtoupper($services)) ? $return = 'http://ibidadmsdevservicehandover.azurewebsites.net/index.php/' : '';
-	$STOCK 		= ('STOCK' == strtoupper($services)) ? $return = 'http://ibidadmsdevservicestock.azurewebsites.net/index.php/' : '';
-	$MASTER 	= ('MASTER' == strtoupper($services)) ? $return = 'http://ibidadmsdevservicemasterdata.azurewebsites.net/index.php/' : '';
-	$FINANCE 	= ('FINANCE' === strtoupper($services)) ? $return = 'http://ibidadmsdevservicefinance.azurewebsites.net/index.php/' : '';
-	$NPL  		= ('NPL' === strtoupper($services)) ? $return = 'http://ibidadmsdevservicenpl.azurewebsites.net/index.php/' : '';
-	$CMS		= ('CMS' === strtoupper($services)) ? $return = 'http://alpha.ibid.astra.co.id/backend/dapur/' : '';
+	// kalo pengen jalan diawanesia komen dibawah di aktifkan saja  
+	$BACKEND = ('BACKEND' == strtoupper($services)) ? $return = 'http://alpha.ibid.astra.co.id/backend/adms/index.php/' : '';
+	$ACCOUNT = ('ACCOUNT' == strtoupper($services)) ? $return = 'http://alpha.ibid.astra.co.id/backend/service/akun/index.php/' : ''; 
+	$NOTIF = ('NOTIF' == strtoupper($services)) ? $return = 'http://alpha.ibid.astra.co.id/backend/service/notif/index.php/' : ''; 
+	$TAKSASI =  ('TAKSASI' == strtoupper($services)) ? $return = 'http://alpha.ibid.astra.co.id/backend/service/taksasi/index.php/' : ''; 
+	$FRONTEND = ('FRONTEND' == strtoupper($services)) ? $return = 'http://alpha.ibid.astra.co.id/index.php/' : '';
+	$HANDOVER = ('HANDOVER' == strtoupper($services)) ? $return = 'http://alpha.ibid.astra.co.id/backend/service/handover/index.php/' : '';
+	$STOCK = ('STOCK' == strtoupper($services)) ? $return = 'http://alpha.ibid.astra.co.id/backend/service/stok/index.php/' : '';
+	$MASTER = ('MASTER' == strtoupper($services)) ? $return = 'http://alpha.ibid.astra.co.id/backend/service/masterdata/index.php/' : '';
+	$FINANCE = ('FINANCE' === strtoupper($services)) ? $return = 'http://alpha.ibid.astra.co.id/backend/service/finance/index.php/' : '';
+	$NPL = ('NPL' === strtoupper($services)) ? $return = 'http://alpha.ibid.astra.co.id/backend/service/npl/index.php/' : '';
+	$CMS = ('CMS' === strtoupper($services)) ? $return = 'http://alpha.ibid.astra.co.id/backend/dapur/' : '';
 
 	return $return;
 }
