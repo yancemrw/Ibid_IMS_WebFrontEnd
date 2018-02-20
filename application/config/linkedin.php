9190ead79d45fb34e5fe04ebb61902a37c0b563f
Tupay <lutfi@mhs.ubharajaya.ac.id>
2018-02-20 10:27:55 +0700

    update linkedin

--- application/config/linkedin.php
+++ application/config/linkedin.php
@@ -7,8 +7,8 @@ $config = array(
 	'linkedin' => array(
 		'API_KEY' => '81o3npu3d7wt4d',
 		'SECRET_KEY' => 'VIfJ9bU6CP017yps',
-		'SITE_URL' => 'http://localhost:8888/lutfi/linkedin/pertama/',
-		'REDIRECT_URL' => 'http://alpha.ibid.astra.co.id/index.php/omni/linkedin/linkedin',  
+		'SITE_URL' => 'http://beta.ibid.astra.co.id/lutfi/linkedin/pertama/',
+		'REDIRECT_URL' => 'http://beta.ibid.astra.co.id/index.php/omni/linkedin/linkedin',  
 		'SCOPE' => 'r_basicprofile r_emailaddress', 
 		)
 	);
