c54d44168a557ac30e3fc89a13639d3a8152e5d1
Andi Fauji Akbar <faujiakbar@gmail.com>
2018-03-20 10:42:02 +0700

    Check OTP Direct to AWO Server

--- application/controllers/Otp.php
+++ application/controllers/Otp.php
@@ -14,4 +14,35 @@ class Otp extends CI_Controller {
 		}
 		echo "Tidak ditemukan";
 	}
+
+	public function check($phone=null){
+		if(!is_null($phone)){
+			if(file_exists($file = APPPATH."../sms/".$phone)){
+				$f = fopen($file, "r");
+				while(! feof($f)){
+					$t = "trxID : ";
+					$txt = explode($t, fgets($f));
+					if(is_array($txt) && count($txt) > 1){
+						$trxID = str_replace("\n", '', $txt[1]);
+						## get detail users
+						$url = linkservice('notif')."api/sms/".$trxID;
+						$responseApi = admsCurl($url);
+						if ($responseApi['err']) { echo "<hr>cURL Error #:" . $responseApi['err']; } else {
+							$sms = json_decode($responseApi['response'],true);
+						}
+						
+						if(isset($sms)){
+							foreach ($sms['data'] as $key => $dt) {
+								echo $key." : ".$dt."<br />";
+							}
+							echo "<br /><br />Notes :<br />";
+							echo "- Status : 0 = queue, 10 = failed, 20 = success, 100 = trxID not found"; 
+							die();
+						}
+					}
+				}
+			}
+		}
+		echo "Tidak ditemukan";
+	}
 }
