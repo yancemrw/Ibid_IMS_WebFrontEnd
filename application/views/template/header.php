81b310a6b9b63a1d019d886accbd9f37aa8e9e85
Anggoro <ankghoro@gmail.com>
2018-04-25 11:50:50 +0700

    Update CSS Schedule

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -194,7 +194,7 @@
                         <li class="nav-close"><img src="<?php echo base_url('assetsfront/images/icon/Close.png'); ?>"></li>
                         <li class="nav-title"><span class="nav-title">Home</span></li>
                         <li id="find_menu"><a href="<?php echo site_url('cari-lelang'); ?>" onclick="setActiveMenu('find')">Cari Kendaraan</a></li>
-                        <li id="schedule_menu"><a href="<?php echo site_url('comingsoon'); ?>" onclick="setActiveMenu('schedule')">Jadwal Lelang</a></li>
+                        <li id="schedule_menu"><a href="<?php echo site_url('auction/Auction_date'); ?>" onclick="setActiveMenu('schedule')">Jadwal Lelang</a></li>
                         <li id="live_menu"><a href="<?php echo site_url('live-auction'); ?>" onclick="setActiveMenu('live')">Live Auction</a></li>
                         <li id="npl_menu"><a href="<?php echo site_url('beli-npl'); ?>" onclick="setActiveMenu('npl')">Beli Npl</a></li>
                         <li id="auction_menu"><a href="<?php echo site_url('titip-lelang'); ?>" onclick="setActiveMenu('auction')">Titip Lelang</a></li>
@@ -254,7 +254,7 @@
                         <li class="nav-close"><img src="<?php echo base_url('assetsfront/images/icon/Close.png'); ?>"></li>
                         <li class="nav-title"><span class="nav-title">Home</span></li>
                         <li id="find_menu"><a href="<?php echo site_url('cari-lelang'); ?>" onclick="setActiveMenu('find')">Cari Kendaraan</a></li>
-                        <li id="schedule_menu"><a href="<?php echo site_url('comingsoon'); ?>" onclick="setActiveMenu('schedule')">Jadwal Lelang</a></li>
+                        <li id="schedule_menu"><a href="<?php echo site_url('auction/Auction_date'); ?>" onclick="setActiveMenu('schedule')">Jadwal Lelang</a></li>
                         <li id="live_menu"><a href="<?php echo site_url('live-auction'); ?>" onclick="setActiveMenu('live')">Live Auction</a></li>
                         <li id="npl_menu"><a href="<?php echo site_url('beli-npl'); ?>" onclick="setActiveMenu('npl')">Beli Npl</a></li>
                         <li id="auction_menu"><a href="<?php echo site_url('titip-lelang'); ?>" onclick="setActiveMenu('auction')">Titip Lelang</a></li>
