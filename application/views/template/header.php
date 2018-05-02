0731e198dcc6b03f8ce634d04a057fb8fb71144f
ankghoro <ankghoro@gmail.com>
2018-05-02 10:28:40 +0700

    Update Permintaan Mas Abi

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -134,6 +134,17 @@
             }, 1000);
          }
       }
+	
+      // Start of Tawk.to Script
+      var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+      (function() {
+	 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+	 s1.async=true;
+	 s1.src='https://embed.tawk.to/5ae83c48227d3d7edc24dc16/default';
+	 s1.charset='UTF-8';
+	 s1.setAttribute('crossorigin','*');
+	 s0.parentNode.insertBefore(s1, s0);
+      })();
 
       // cek compare panel
       if(localStorage.getItem("CP") !== null) {
