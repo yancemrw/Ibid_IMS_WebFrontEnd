f87f4bae8b45fbe8176a82cf5d9259b76ac54313
Anggoro <ankghoro@gmail.com>
2018-05-16 17:03:46 +0700

    Update CSS Blog

--- application/controllers/about/Blog.php
+++ application/controllers/about/Blog.php
@@ -6,16 +6,22 @@ class Blog extends CI_Controller {
 	public function __construct() {
 		parent::__construct();
 		$this->load->helper(array('global', 'omni'));
+		$this->userdata = $this->session->userdata('userdata');
 	}
 
 	public function index() {
-		$userdata = $this->session->userdata('userdata');
+		$url = linkservice('cms')."api/home";
+		$method = 'GET';
+		$res = admsCurl($url, array(), $method);
+		$generate = curlGenerate($res);
+
 		$data = array(
 			'header_white'		=> "header-white",
-			'userdata'			=> $userdata,
+			'userdata'			=> $this->userdata,
 			'title'				=> 'Tentang IBID',
 			'form_auth_mobile'	=> login_status_form_mobile($userdata),
-			'form_auth'			=> login_Status_form($userdata)
+			'form_auth'			=> login_Status_form($userdata),
+			'content'			=> $generate
 		);
 		$view = "about/blog";
 		template($view, $data);
