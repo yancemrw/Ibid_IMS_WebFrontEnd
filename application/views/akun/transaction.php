a3dcd867fabf338ea6d14baf93ea66b7a0e477cd
Anggoro <ankghoro@gmail.com>
2018-05-19 02:16:23 +0700

    Ketinggalan

--- application/views/akun/transaction.php
+++ application/views/akun/transaction.php
@@ -1,5 +1,5 @@
-<link rel="stylesheet" type="text/css" href="<?php echo base_url('assetsfront/datatables/datatables.css'); ?>">
-<script type="text/javascript" src="<?php echo base_url('assetsfront/datatables/datatables.js'); ?>"></script>
+<link rel="stylesheet" type="text/css" href="<?php echo base_url('assetsfront/datatables/css/jquery.dataTables.css'); ?>" />
+<script type="text/javascript" src="<?php echo base_url('assetsfront/datatables/js/jquery.dataTables.js'); ?>"></script>
 <section>
     <div class="container-fluid">
         <div class="row position-repative">
@@ -135,7 +135,7 @@
                                 </form>
                             </div>
                             <div class="table-responsive table-container table-transaction">
-                                <table id="table-beli" class="table table-striped table-custom table-custom-transaction table-responsive-am">
+                                <table id="table-beli" class="table table-striped table-custom table-custom-transaction table-responsive-am width-100">
                                     <thead>
                                         <tr>
                                             <th>No</th>
@@ -164,24 +164,24 @@
     var tableJual = $('#table-jual').DataTable({
         "dom": 'rt<"bottom"ip><"clear">',
         "bLengthChange": false,
-        "bFilter": true,
-        "fnServerParams": function ( aoData ) {
+        "fnServerParams": function (aoData) {
             aoData.push( {"name":"changeJual", "value":$('#changeJual').val()} );
         },
         "language": {
-            'emptyTable': '<b class="text-shadows">Tidak Ada Penjualan / Titip Lelang</b>'
-        }
+            'emptyTable': 'Tidak Ada Penjualan / Titip Lelang'
+        },
+        "ajax": "<?php echo linkservice('frontend').'akun/Transaction/getWinnerDatatables/jual'; ?>"
     });
     var tableBeli = $('#table-beli').DataTable({
         "dom": 'rt<"bottom"ip><"clear">',
         "bLengthChange": false,
-        "bFilter": true,
-        "fnServerParams": function ( aoData ) {
+        "fnServerParams": function (aoData) {
             aoData.push( {"name":"changeBeli", "value":$('#changeBeli').val()} );
         },
         "language": {
-            'emptyTable': '<b class="text-shadows">Tidak Ada Pembelian</b>'
-        }
+            'emptyTable': 'Tidak Ada Pembelian'
+        },
+        "ajax": "<?php echo linkservice('frontend').'akun/transaction/getWinnerDatatables/beli'; ?>"
     });
     $('input[name="filterJual"]').keypress(function(e) {
         if(e.which == 13) {
@@ -193,151 +193,6 @@
         if(e.which == 13) {
             e.preventDefault();
             tableBeli.search(this.value).draw();
-        }
-    });
-    $.ajax({
-        type: 'GET',
-        url: "<?php echo linkservice('stock').'winner/get'; ?>",
-        data: {
-			pemilik: '<?php echo $userdata['UserId'] ?>'
-		},
-        beforeSend: function() {
-            $('#table-jual tbody').html('<tr><td colspan="7"><b class="text-shadows">Memuat Data...</b></td></tr>');
-        },
-        success: function(data) {
-			var data = data.data, rows;
-            for(var i = 0; i < data.length; i++) {
-				
-				keteranganStatus = 'Belum Dilunasi';
-				imgStep_1 = '<?php echo base_url('assetsfront/images/icon/ic_transaction_step_1_grey.png'); ?>';
-				imgStep_2 = '<?php echo base_url('assetsfront/images/icon/ic_transaction_step_2_grey.png'); ?>';
-				imgStep_3 = '<?php echo base_url('assetsfront/images/icon/ic_transaction_step_3_grey.png'); ?>';
-				imgStep_4 = '<?php echo base_url('assetsfront/images/icon/ic_transaction_step_4_grey.png'); ?>';
-				if (data[i].IsPaid == 1){
-					imgStep_1 = '<?php echo base_url('assetsfront/images/icon/ic_transaction_step_1.png'); ?>';
-					keteranganStatus = '';
-					if (data[i].IsPaid == 1){
-						
-					}
-				}
-				
-                rows += '<tr>'+
-                        '<td></td>'+
-                        '<td>'+data[i].nopolisi+'</td>'+
-						'<td>'+data[i].merk+' '+data[i].seri+' '+data[i].silinder+' '+data[i].model+' '+data[i].transmisi+'<br>'+data[i].tahun+'</td>'+
-                        '<td>Jenis</td>'+
-                        '<td>Rp. '+currency_format(data[i].Billing)+'</td>'+
-                        '<td>'+data[i].ScheduleAuctionWinnerCompany+'<br>'+data[i].ScheduleAuctionWinnerDate+'</td>'+
-                        '<td>'+
-						'<a href="" class="step-transaction">'+
-						'<ul>'+
-						'<li><img class="imgSrcStep-1-'+data[i].AuctionItemId+'" src="'+imgStep_1+'" alt=""></li>'+
-						'<li><img class="imgSrcStep-2-'+data[i].AuctionItemId+'" src="<?php echo base_url('assetsfront/images/icon/ic_transaction_step_2_grey.png'); ?>" alt=""></li>'+
-						'<li><img class="imgSrcStep-3-'+data[i].AuctionItemId+'" src="<?php echo base_url('assetsfront/images/icon/ic_transaction_step_3_grey.png'); ?>" alt=""></li>'+
-						'<li><img class="imgSrcStep-4-'+data[i].AuctionItemId+'" src="<?php echo base_url('assetsfront/images/icon/ic_transaction_step_4_grey.png'); ?>" alt=""></li>'+
-						'<p>Serah Terima Kendaraan Kepada Pemenang</p>'+
-						'</ul>'+
-						'</a>'+
-						'</td>'+
-                        '</tr>';
-            }
-            $('#table-jual tbody tr').replaceWith(rows);
-		},
-		complete: function(data){
-			// data = data.responseJSON.data;
-			// setTimeout(function(){ 
-				// alert("Hello"); 
-				// console.log(data); 
-			// }, 1000);
-			
-		},
-        error: function(e) {
-            $('#table-jual tbody tr').replaceWith('<tr><td colspan="7"><b class="text-shadows">Tidak Ada Penjualan / Titip Lelang</b></td></tr>');
-            /*bootoast.toast({
-                message: "Gagal Mengambil Data",
-                type: 'warning',
-                position: 'top-center',
-                timeout: 4
-            });*/
-        }
-    });
-	
-	$.ajax({
-        type: 'GET',
-        url: "<?php echo linkservice('stock').'winner/get'; ?>",
-        data: {
-			userid: '<?php echo $userdata['UserId'] ?>'
-		},
-        beforeSend: function() {
-            $('#table-beli tbody').html('<tr><td colspan="7"><b class="text-shadows">Memuat Data...</b></td></tr>');
-        },
-        success: function(data) {
-			var data = data.data, rows;
-            for(var i = 0; i < data.length; i++) {
-				
-				keteranganStatus = 'Belum Dilunasi';
-				imgStep_1 = '<?php echo base_url('assetsfront/images/icon/ic_transaction_step_1_grey.png'); ?>';
-				imgStep_2 = '<?php echo base_url('assetsfront/images/icon/ic_transaction_step_2_grey.png'); ?>';
-				imgStep_3 = '<?php echo base_url('assetsfront/images/icon/ic_transaction_step_3_grey.png'); ?>';
-				imgStep_4 = '<?php echo base_url('assetsfront/images/icon/ic_transaction_step_4_grey.png'); ?>';
-				
-				
-				if (data[i].IsPaid == 1){
-					imgStep_1 = '<?php echo base_url('assetsfront/images/icon/ic_transaction_step_1.png'); ?>';
-					keteranganStatus = '';
-					if (data[i].IsPaid == 1){
-						
-					}
-				}
-				
-				if (data[i].IsTakeOutSPPU == 1){
-					keteranganStatus = keteranganStatus + '<br>Unit Sudah diambil';
-				} else {
-					keteranganStatus = keteranganStatus + '<br>Unit Belum diambil';
-				}
-				
-				keteranganStatus = keteranganStatus + '<br>Dokumen Belum diambil';
-				
-                rows += '<tr>'+
-                        '<td></td>'+
-                        '<td>'+data[i].NPLNumber+'</td>'+
-                        '<td>'+data[i].nopolisi+'</td>'+
-						'<td>'+data[i].merk+' '+data[i].seri+' '+data[i].silinder+' '+data[i].model+' '+data[i].transmisi+'<br>'+data[i].tahun+'</td>'+
-                        '<td>Jenis</td>'+
-                        '<td>Rp. '+currency_format(data[i].Billing)+'</td>'+
-                        '<td>'+data[i].ScheduleAuctionWinnerCompany+'<br>'+data[i].ScheduleAuctionWinnerDate+'</td>'+
-                        '<td>'+
-						'<a href="" class="step-transaction">'+
-						'<p>'+keteranganStatus+'</p>'+
-						// '<ul>'+
-						// '<li><img class="imgSrcStep-1-'+data[i].AuctionItemId+'" src="'+imgStep_1+'" alt=""></li>'+
-						// '<li><img class="imgSrcStep-2-'+data[i].AuctionItemId+'" src="'+imgStep_2+'" alt=""></li>'+
-						// '<li><img class="imgSrcStep-3-'+data[i].AuctionItemId+'" src="'+imgStep_3+'" alt=""></li>'+
-						// '<li><img class="imgSrcStep-4-'+data[i].AuctionItemId+'" src="'+imgStep_4+'" alt=""></li>'+
-						// '</ul>'+
-						// '<p>Serah Terima Kendaraan Kepada Pemenang</p>'+
-						'</a>'+
-						'</td>'+
-                        '</tr>';
-            }
-            $('#table-beli tbody tr').replaceWith(rows);
-		},
-		complete: function(data){
-			// data = data.responseJSON.data;
-			// setTimeout(function(){ 
-				// alert("Hello"); 
-				// console.log(data); 
-			// }, 1000);
-			
-		},
-        error: function() {
-            $('#table-beli tbody tr').replaceWith('<tr><td colspan="8"><b class="text-shadows">Tidak Ada Pembelian</b></td></tr>');
-            /*bootoast.toast({
-                message: "Gagal Mengambil Data",
-                type: 'warning',
-                position: 'top-center',
-                timeout: 4
-            });*/
         }
     });
 
