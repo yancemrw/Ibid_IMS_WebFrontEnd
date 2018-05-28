a409bd45aabcc5bca90521840328384596e199f4
Anggoro <ankghoro@gmail.com>
2018-05-28 16:27:16 +0700

    Add Landing Page

---
+++ application/controllers/Landing.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Landing extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('Landing');
+	}
+
+}
+
+/* End of file Landing.php */
+/* Location: ./application/controllers/Landing.php */
