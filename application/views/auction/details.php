e1a1c0001d3f2468f2037fe0a2c0bd4e604d0c10
ankghoro <ankghoro@gmail.com>
2018-06-09 15:42:31 +0700

    update

--- application/views/auction/details.php
+++ application/views/auction/details.php
@@ -163,6 +163,14 @@
 </style>
 
 <script>
+// sementara
+bootoast.toast({
+	message: "Kami Mohon Maaf Karena Terdapat Kesalahan Teknis, Mohon Maaf Atas Tidak Nyaman ini",
+	type: 'warning',
+	position: 'top-center',
+	timeout: 5
+});
+	
 myFav = [];
 <?php foreach($favorite as $row){ ?>
 myFav.push(<?php echo $row->AuctionItemId; ?>);
