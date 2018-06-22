b245aff61d396833599ea83c4740e8b970b9cdda
ankghoro <ankghoro@gmail.com>
2018-06-22 22:09:02 +0700

    Update Versioning CSS

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -52,15 +52,15 @@
    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assetsfront/font-awesome/css/font-awesome.min.css') ?>">
    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assetsfront/css/select2.min.css') ?>">
    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assetsfront/css/fullcalendar.min.css') ?>">
-   <link rel="stylesheet" type="text/css" href="<?php echo base_url('assetsfront/css/style.css?v3') ?>">
-   <link rel="stylesheet" type="text/css" href="<?php echo base_url('assetsfront/css/responsive.css?v4') ?>">
+   <link rel="stylesheet" type="text/css" href="<?php echo base_url('assetsfront/css/style.css?v4') ?>">
+   <link rel="stylesheet" type="text/css" href="<?php echo base_url('assetsfront/css/responsive.css?v5') ?>">
    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assetsfront/css/slick.css') ?>">
    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assetsfront/css/lightgallery.min.css') ?>">
    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assetsfront/css/image-sprite-style.css?v1') ?>">
    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assetsfront/css/prettify.css') ?>">
    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assetsfront/css/jquery.flipster.min.css'); ?>">
    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assetsfront/bootoast/bootoast.css'); ?>">
-   <link rel="stylesheet" type="text/css" href="<?php echo base_url('assetsfront/css/overwrite.css?v7'); ?>">
+   <link rel="stylesheet" type="text/css" href="<?php echo base_url('assetsfront/css/overwrite.css?v8'); ?>">
 
    <!-- Header Js -->
    <script>var img_empty = "<?php echo base_url('assetsfront/images/background/management-empty.png'); ?>";</script>
