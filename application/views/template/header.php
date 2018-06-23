4b48a59870f846dc82343fb6f205858c84347bc3
ankghoro <ankghoro@gmail.com>
2018-06-23 11:55:59 +0700

    Add Google Tag Manager

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -178,8 +178,23 @@
          document.cookie = name+'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
       }
    </script>
+	
+   <!-- Google Tag Manager -->
+   <script>
+   (function(w,d,s,l,i){
+      w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+      var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+      j.async=true;j.src = 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+   })(window,document,'script','dataLayer','GTM-NLSP55K');
+   </script>
+   <!-- End Google Tag Manager -->
 </head>
 <body class="bg-grey">
+   <!-- Google Tag Manager (noscript) -->
+   <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NLSP55K" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+   <!-- End Google Tag Manager (noscript) -->
+	
    <!-- handle header between procedure page and other page -->
    <?php if(@$menu_pages) { ?>
       <header class="header-min <?php echo $class_header; ?>" style="background: url(<?php echo $bgheader; ?>)no-repeat fixed;
