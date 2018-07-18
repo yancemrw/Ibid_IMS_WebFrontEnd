59a08dad2272fb7558c5cc0d1403dcb134711fdd
ankghoro <ankghoro@gmail.com>
2018-07-18 10:51:41 +0700

    Update Link Handle

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -764,7 +764,7 @@ function isreadpage(ele, UserId, key, isread) {
 }
 
 function detectProtocol(url) {
-  return "http:"+url;
+  return ((url.indexOf('http:') !== -1 || url.indexOf('https:') !== -1) ? url : ('http:'+url));
 }
 </script>
 </body>
